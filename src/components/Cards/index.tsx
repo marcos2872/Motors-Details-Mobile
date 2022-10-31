9dde76266ed84ec1c1988ba24e3d4ec4c288c263
Marcos Souza Brito <njsu5@hotmail.com>
2022-10-31 14:31:32 -0300

    card dados mokados

--- src/components/Cards/index.tsx
+++ src/components/Cards/index.tsx
@@ -1,20 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Text } from 'react-native'
-import { Card, Scroll, Container } from './styled'
+import getMotors from '../../utils/getAllMotors';
+import { Card, Scroll, Container, Name, Image } from './styled';
+import mokMotors from '../../utils/mokApi'
+
+type motoType =
+  | {
+      _id: string;
+      model: string;
+      tension: number;
+      magnets: number;
+      poles: number;
+      potency: string;
+      wire: string;
+      turns: number;
+      'parallel-wire': number;
+      'wire-length': string;
+      images: {
+        key: string;
+        url: string;
+      }[];
+      video: string;
+    }[]
+  | undefined;
 
 const styled = () => {
+  const [cards, setCards] = useState<motoType>([])
+
+  useEffect(() => {
+    (async () => {
+      // const motors = await getMotors()
+      setCards(mokMotors)
+    })()
+  }, [])
   return (
     <Container>
       <Scroll showsVerticalScrollIndicator={false}>
-        <Card>
+        {cards.map((motor) => (
+          <Card key={Math.random()}>
+            <Image source={{
+          uri: motor.images[0].url,
+        }}/>
+            <Name>{motor.model}</Name>
+          </Card>
+        ))}
 
-        </Card>
-
-        <Card><Text>tttttttt</Text></Card>
-
-        <Card><Text>tttttttt</Text></Card>
-
-        <Card><Text>tttttttt</Text></Card>
       </Scroll>
     </Container>
 
--- src/components/Cards/styled.ts
+++ src/components/Cards/styled.ts
@@ -15,4 +15,18 @@ width: 100%;
 height: 200px;
 border-radius: 10px;
 margin: 5px 0;
+align-items: center;
+justify-content: space-around;
+flex-direction: row;
+padding: 5px;
+`
+export const Name = styled.Text`
+font-family: ${({theme}) => theme.FONTS.MEDIUM};
+`
+
+export const Image = styled.Image`
+background-color: aqua;
+border-radius: 10px;
+width: 60%;
+height: 80%;
 `
